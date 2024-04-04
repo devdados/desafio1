cd034b9b1c62752c31df28c15167df88850e0444
Andréa <rangelfoco2@gmail.com>
2024-04-04 15:50:43 -0300

    desafio-componentizacao

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import '../../../src/App.css'
+
+function Header() {
+    return (
+        <>
+            <h1>Eu sou uma frase bem bonita</h1>
+        </>
+    )
+}
+
+export default Header
